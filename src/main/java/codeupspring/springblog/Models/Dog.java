1ff57bcde67f70a6fd275e75c903f064fbfb2fec
Ryan McGuire <ryan.wayne.mcguire@gmail.com>
2021-02-11 22:37:47 -0600

    views exercise and mini table creation exercise

---
+++ src/main/java/codeupspring/springblog/Models/Dog.java
@@ -0,0 +1,59 @@
+package codeupspring.springblog.Models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="dogs")
+public class Dog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, length = 100)
+    private int age;
+
+    @Column(nullable = false, length = 100)
+    private String resideState;
+
+    public Dog(long id, String name, int age, String resideState){
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.resideState = resideState;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getResideState() {
+        return resideState;
+    }
+
+    public void setResideState(String resideState) {
+        this.resideState = resideState;
+    }
+}
--- src/main/java/codeupspring/springblog/controllers/HomeController.java
+++ src/main/java/codeupspring/springblog/controllers/HomeController.java
@@ -2,6 +2,7 @@ package codeupspring.springblog.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -11,5 +12,4 @@ public class HomeController {
     public String landingPage(){
        return "This is the landing page";
    }
-
 }
